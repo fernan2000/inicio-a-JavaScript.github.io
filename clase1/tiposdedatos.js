5f6a4614f635eb23e5c8b8f67e8f8e0e681de064
vasti <vati050606@gmail.com>
2026-01-27 20:22:49 -0600

    primer commit

---
+++ clase1/tiposdedatos.js
@@ -0,0 +1,34 @@
+//tipos datos
+
+//strings
+
+"hola soy un string"
+'yo tambien soy un string'
+
+console.log(typeof 'diecinueve'); // string
+console.log(typeof 'martes'); // string
+// tipo numero//
+2
+1.1
+-1
+
+console.log(typeof 42);    
+console.log(typeof -469);     
+console.log(typeof 0);  
+
+//tipo booleano//
+true
+false
+
+console.log(typeof true);       // booleano
+console.log(typeof false);     // booleano
+//tipo undefined//
+undefined
+
+console.log(typeof undefined);  // undefined
+
+console.log(typeof "esto es un string")
+
+//adicionales//
+console.log(typeof 3.14159265);       // number (decimal)
+console.log(typeof 'clase 1');   // string
