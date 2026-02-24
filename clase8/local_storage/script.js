c29e1f75055f8b7213ffa22eb203da50ebd1410a
vasti <vati050606@gmail.com>
2026-02-24 08:28:13 -0600

    clase7

---
+++ clase8/local_storage/script.js
@@ -0,0 +1,7 @@
+const comment_input = document.getElementById('comment_input');
+const save_button = document.getElementById('save_button');
+const comment_box = document.getElementById('comment_box');
+
+save_button.addEventListener('click', () => {
+    const 
+})
